6820b1cccc7133e3e220a6f730fb33d0b34d2232
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-25 19:06:32 -0400

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,63 @@ import React from "react";
 
 const Header = () => {
   return (
-    <section className="container">
-      <div className="flex justify-between">
-        <div className="">LOGO</div>
-        <div className="">SEARCH</div>
-        <div className="">CURRENT LOCATION BUTTON</div>
+    <header className="sticky top-0 backdrop-blur-md z-20">
+      <div className="container">
+        <div className="flex flex-col md:flex-row gap-3 justify-between py-5 md:items-center w-full">
+          <div className="font-semibold">WEATHER.IO</div>
+          <div className="">
+            <form className="md:w-[450px]">
+              <label
+                // for="default-search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              >
+                Search
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search any location"
+                  required
+                />
+                <button
+                  type="submit"
+                  className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Search
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="">
+            <button
+              type="button"
+              className="py-2.5 px-5 text-sm font-medium text-black focus:outline-none bg-gray-400 rounded-full border border-gray-400 hover:bg-gray-300"
+            >
+              Current Location
+            </button>
+          </div>
+        </div>
       </div>
-    </section>
+    </header>
   );
 };
 
